96fd00a594bccf6ef026817c70846e4bf780371e
Kuntinun Onvinijvorakul <sundaypapa25@gmail.com>
2024-04-05 15:19:43 +0700

    fix Main page

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var footer = document.getElementsByTagName("footer")[0];
 
 window.onresize = function (){
     let width = window.innerWidth;
-    if(width < 640){ footer.style.display="none"}
-    else{ footer.style.display="block"}
+    if(width < 640){ footer.style.display="block"}
+    else{ footer.style.display="none"}
 }
